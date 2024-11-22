e343df565d7cdbb36866e5a249435a3a3a3edd28
juanito-alimanha <juanjosea198@gmail.com>
2024-11-21 21:54:49 -0500

    Update 01-fundamentos.js

---
+++ javascript/01-fundamentos.js
@@ -0,0 +1,61 @@
+// Esto es un comentario de JavaScript de una sola línea
+
+/*
+Esto es un comentario
+de JavaScript
+en varias líneas
+*/
+
+// en la siguiente línea estoy imprimiento un valor en consola
+// console.log('hola, a todos!');
+
+/* VARIABLES */
+// Declaración
+var nombre1;
+let nombre1a;
+
+// declaración e inicialización
+var nombre2 = 'pepita';
+let nombre2a = 'pepito';
+
+// variable de tipo constante
+const miConstante = 1;
+// console.log(miConstante);
+
+// automática
+// miVariableAutomatica;
+
+let miVariable = 'hola';
+// console.log(miVariable);
+miVariable = 'hello'; 
+// console.log(miVariable);
+
+/* TIPOS DE DATO PRIMITIVOS */
+miVariable = 'Hola, otra vez!';
+miVariable = "I'm";
+miVariable = 'Él dijo: "Hola"';
+miVariable = `Ella dijo: ${nombre2}`;
+miVariable = 'Él pensó en: ' + nombre2;
+miVariable = '1';
+miVariable = 'true';
+miVariable = '';
+
+miVariable = 0;
+miVariable = 10;
+miVariable = 9.5;
+miVariable = 0xff;
+miVariable = 123e5;
+miVariable = -10;
+
+miVariable = true;
+miVariable = false;
+
+miVariable = undefined;
+console.log(miVariable);
+
+/* FUNCIONES */
+function miFuncion() {
+    console.log('Pepita Pérez');
+  }
+  
+  miFuncion();
