0e8094278f24ff76cd25882eb5773c24ba41e90b
juanito-alimanha <juanjosea198@gmail.com>
2024-11-21 21:55:39 -0500

    Update desafios.js

--- javascript/desafios.js
+++ javascript/desafios.js
@@ -38,13 +38,11 @@ console.log(`Hola, soy ${nombre} y tengo ${edad} años.`);
 }
 presentacion('Juan Jose',edad);*/
 
-
-
 /* 4. Crear una función que reciba un nombre, un apellido y una edad por parámetros,
 la función debe imprimir en consola: Su nombre es ____ y usted tiene ____ años,
 si no se recibe la edad imprimir: Su nombre es ____ y usted tiene 100 años.*/
 
-function presentacion (nombre, apellido, edad = 100){
+/*function presentacion (nombre, apellido, edad = 100){
 console.log(`Su nombre es ${nombre} ${apellido} y usted tiene ${edad} años.`);
 }
-presentacion('Alberto','Gutierrez');
+presentacion('Alberto','Gutierrez');*/
